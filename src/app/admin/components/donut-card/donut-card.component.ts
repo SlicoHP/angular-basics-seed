dbb9b040d15a048480221ceaf5424a02235f00ce
SlicoHP <rodrigojimenezgarcia87@gmail.com>
2022-09-21 20:44:05 +0200

    Use a pipe to format currency

---
+++ src/app/admin/components/donut-card/donut-card.component.ts
@@ -0,0 +1,58 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Donut } from '../../models/donut.model';
+
+@Component({
+  selector: 'donut-card',
+  template: `
+    <div
+      class="donut-card"
+      [ngClass]="{
+        'donut-card-promo': donut.promo
+      }"
+    >
+      <img
+        src="/assets/img/{{ donut.icon }}.svg"
+        [alt]="donut.name"
+        class="donut-card-icon"
+      />
+      <div>
+        <p class="donut-card-name">{{ donut.name }}</p>
+        <p class="donut-card-price">{{ donut.price / 100 | currency }}</p>
+      </div>
+    </div>
+  `,
+  styles: [
+    `
+      .donut-card {
+        display: block;
+        display: flex;
+        align-items: center;
+        background: #f7f7f7;
+        border-radius: 5px;
+        margin-bottom: 5px;
+        padding: 5px 15px;
+        transition: transform 0.2s ease-in-out;
+        &:hover {
+          transform: translateY(-3px);
+        }
+        &-name {
+          font-size: 16px;
+        }
+        &-price {
+          font-size: 14px;
+          color: #c14583;
+        }
+        &-icon {
+          width: 50px;
+          margin-right: 10px;
+        }
+        &-promo{
+          border: 2px solid #eee;
+        }
+      }
+    `,
+  ],
+})
+export class DonutCardComponent {
+  @Input() donut!: Donut;
+}
--- src/app/admin/containers/donut-list/donut-list.component.ts
+++ src/app/admin/containers/donut-list/donut-list.component.ts
@@ -1,20 +1,50 @@
 import { Component, OnInit } from '@angular/core';
+import { Donut } from '../../models/donut.model';
 
 @Component({
   selector: 'donut-list',
   template: `
-    <p>
-      donut-list works!
-    </p>
+    <div>
+      <donut-card [donut]="donuts[0]"></donut-card>
+      <donut-card [donut]="donuts[1]"></donut-card>
+      <donut-card [donut]="donuts[2]"></donut-card>
+    </div>
   `,
   styles: [
+   
   ]
 })
 export class DonutListComponent implements OnInit {
+  donuts!: Donut[]
 
   constructor() { }
 
   ngOnInit(): void {
+    this.donuts = [
+      {
+        id: 'y2jw23',
+        name: 'Just Chocolate',
+        icon: 'just-chocolate',
+        price: 119,
+        description: 'For the pure chocoholic.'
+      },
+      {
+        id: '82uwj3',
+        name: 'Glazed Fudge',
+        icon: 'glazed-fudge',
+        price: 129,
+        promo: true,
+        description: 'Sticky perfection.'
+      },
+      {
+        id: '823jeh',
+        name: 'Caramel Swirl',
+        icon: 'caramel-swirl',
+        price: 232,
+        description: 'Chocolate drizzed.'
+      }
+    ];
+
   }
 
 }
