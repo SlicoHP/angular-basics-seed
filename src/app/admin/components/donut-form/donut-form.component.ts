36ca20914ac3b1e64a71deca084a65f9e26e0582
SlicoHP <rodrigojimenezgarcia87@gmail.com>
2022-09-25 20:11:55 +0200

    NgModel template refs and validation states

--- src/app/admin/components/donut-form/donut-form.component.ts
+++ src/app/admin/components/donut-form/donut-form.component.ts
@@ -6,7 +6,13 @@ import { Component, OnInit } from '@angular/core';
     <form class="donut-form" #form="ngForm">
       <label>
         <span>Name</span>
-        <input type="text" name="name" class="input" required ngModel />
+        <input type="text" name="name" class="input" required ngModel #name="ngModel" />
+        <p>{{name.valid}}</p>
+        <p>{{name.invalid}}</p>
+        <p>{{name.touched}}</p>
+        <p>{{name.untouched}}</p>
+        <p>{{name.pristine}}</p>
+        <p>{{name.dirty}}</p>
       </label>
 
       <label>
@@ -42,7 +48,7 @@ import { Component, OnInit } from '@angular/core';
         <textarea name="description" class="input input--textarea" required ngModel></textarea>
       </label>
 
-      <pre>{{ form.form.status | json }}</pre>
+      <pre>{{ form.value | json }}</pre>
     </form>
   `,
   styles: [
