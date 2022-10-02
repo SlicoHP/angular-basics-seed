74793b1dfcfb2292de03bb0d1bccae5456401936
SlicoHP <rodrigojimenezgarcia87@gmail.com>
2022-10-02 14:07:24 +0200

    End forms and Create an @Injectable Service

--- src/app/admin/containers/donut-single/donut-single.component.ts
+++ src/app/admin/containers/donut-single/donut-single.component.ts
@@ -5,17 +5,25 @@ import { Donut } from '../../models/donut.model';
   selector: 'donut-single',
   template: `
     <div>
-      <donut-form (create)="onCreate($event)"></donut-form>
+      <donut-form [donut]="donut" (create)="onCreate($event)"></donut-form>
     </div>
   `,
   styles: [
   ]
 })
 export class DonutSingleComponent implements OnInit {
+  donut!: Donut;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.donut = {
+      id: 'y2jw23',
+      name: 'Just Chocolate',
+      icon: 'just-chocolate',
+      price: 119,
+      description: 'For the pure chocoholic.',
+    };
   }
 
   onCreate(donut: Donut){
